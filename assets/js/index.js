2c09752d3ed8289ad02027bb3ab43f6ee7d4c5cb
Russell Frankpitt <rustyff123@gmail.com>
2024-07-23 23:46:15 +0200

    refactor of html for testing. add button disabled on empty form

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,21 @@ import "../css/styles.css";
 const menuButton = document.querySelector("#menu-btn");
 const addButton = document.querySelector("#add-project");
 const cancelButton = document.querySelector("#cancel-btn");
+const form = document.querySelector("#form-data");
+const projectName = document.querySelector("#title");
 
 let nextKey = 1;
+
+projectName.addEventListener("change", (e) => {
+    const add = document.querySelector("#add-new-project");
+    const inputText = e.target.value;
+    if (inputText !== "") {
+        add.classList.remove("disabled-btn");
+    } else {
+        add.classList.add("disabled-btn");
+    }
+});
+
 menuButton.addEventListener("click", () => {
     const navContainer = document.querySelector(".nav-container");
     if (navContainer.classList.contains("active")) {
@@ -65,6 +78,34 @@ document
         }
     });
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    let title;
+    let description;
+    let priority;
+    for (const entry of data.entries()) {
+        if (entry[0] === "title") {
+            title = entry[1];
+        } else if (entry[0] === "description") {
+            description = entry[1];
+        } else if (entry[0] === "priority") {
+            priority = entry[1];
+        }
+    }
+    console.log(
+        `Title: ${title}, Description: ${description}, Priority ${priority}`
+    );
+    const newProject = new Projects(
+        `${title}`,
+        `${description}`,
+        `${priority}`,
+        "",
+        (nextKey += 1)
+    );
+    newProject.render();
+});
+
 /* const project1 = new Projects(
     "This",
     "is an example of a project where it has a long description, is an example of a project where it has a long description",
@@ -81,3 +122,13 @@ document
 ); */
 /* project1.render();
 project2.render(); */
+
+/*
+const title = document.querySelector("#title").value;
+    const description = document.querySelector("#description").value;
+    const priority = document.querySelector()
+    console.log(``);
+    const key = nextKey + 1;
+    const newProject = new Projects(title, description, "", "", key);
+    newProject.render();
+*/
